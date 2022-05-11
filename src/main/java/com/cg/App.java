96eba2be329b905cecb468e38a63ae1adc8d7b85
albin-trainer <albinxavier@gmail.com>
2022-05-11 18:02:09 +0530

    2nd commit

--- src/main/java/com/cg/App.java
+++ src/main/java/com/cg/App.java
@@ -10,6 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Good Evening ...");
+        System.out.println("Hv a wonderful evening");
+        System.out.println("Learning Devops");
     }
     
     public int add(int a,int b) {
