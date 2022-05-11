891034e8e8c530a45c60b2d9a940be95c4004b8c
albin-trainer <albinxavier@gmail.com>
2022-05-11 17:39:10 +0530

    1st commit

---
+++ src/main/java/com/cg/App.java
@@ -0,0 +1,19 @@
+package com.cg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Good Evening ...");
+    }
+    
+    public int add(int a,int b) {
+    	return a+b;
+    }
+    
+}
